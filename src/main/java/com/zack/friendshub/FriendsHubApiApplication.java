f62bb11f0b3b86bf424c9c684616a332ee524610
Andrii <zakordonskyii@gmail.com>
2025-11-21 17:43:13 +0100

    Initial Spring Boot project

---
+++ src/main/java/com/zack/friendshub/FriendsHubApiApplication.java
@@ -0,0 +1,13 @@
+package com.zack.friendshub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FriendsHubApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FriendsHubApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zack/friendshub/FriendsHubApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zack.friendshub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FriendsHubApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
