78d3d1805729b6392ff09ffd2e8f00a15af53dd1
Andrii <zakordonskyii@gmail.com>
2025-11-30 22:31:04 +0100

    Refactoring UserStatus, adding logs

--- src/main/java/com/zack/friendshub/controller/UserController.java
+++ src/main/java/com/zack/friendshub/controller/UserController.java
@@ -13,7 +13,6 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 
 @RestController
@@ -75,5 +74,4 @@ public class UserController {
 
         return ResponseEntity.noContent().build();
     }
-
 }
